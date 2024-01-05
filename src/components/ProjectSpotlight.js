b5677176c4090a074467b8615851af12400828d9
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-05 15:43:11 -0800

    Update project spotlight to use Map and pass in key

--- src/components/ProjectSpotlight.js
+++ src/components/ProjectSpotlight.js
@@ -2,20 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function ProjectSpotlight(props) {
-  
+
   let pagesLink;
   let coAuthorList;
   if (props.linkPages) {
     pagesLink = <p className="pagesLink">GH Pages Link: <a href={props.linkPages}>{props.linkPages.slice(8)}</a></p>
   }
   if (props.coAuthors) {
-    coAuthorList = 
-    <React.Fragment>
-      <h3>Co-Authors:</h3>
-      <ul>
-        {props.coAuthors.forEach((author) => <li><a href={author}>{author.slice(19)}</a></li>)}
-      </ul>
-    </React.Fragment>
+    coAuthorList =
+      <React.Fragment>
+        <h3>Co-Authors:</h3>
+        <ul>
+          {props.coAuthors.map((author, index) =>
+            <li key={index}><a href={author}>{author.slice(19)}</a></li>
+          )}
+        </ul>
+      </React.Fragment>
   }
 
   return (
@@ -26,14 +28,14 @@ export default function ProjectSpotlight(props) {
       {pagesLink}
       <h3>Technologies Used:</h3>
       <ol>
-        {props.techsUsed.forEach((feature) => 
-        <li>{feature}</li>
+        {props.techsUsed.map((tech, index) =>
+          <li key={props.title + tech + index}>{tech}</li>
         )}
       </ol>
       <h3>Features:</h3>
       <ul>
-        {props.features.forEach((feature) => 
-        <li>{feature}</li>
+        {props.features.map((feature, index) =>
+          <li key={index}>{feature}</li>
         )}
       </ul>
       {coAuthorList}
