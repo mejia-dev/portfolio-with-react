4eeda0f98553376bbe81a87aa5dda2de9b494d62
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-05 10:55:35 -0800

    Add expanding form to all navBar elements

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,29 +1,41 @@
 import React from "react";
+import '../styles/Portfolio.css'
 
 export default function Portfolio() {
+
+  function expandNavBarItem(elementId) {
+    document.getElementById(elementId).classList.toggle("active");
+    const content = document.getElementById(elementId).nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+
   return (
     <React.Fragment>
-      <div class="navBar">
-        <img class="name" src="img/name.png" alt="Aaron Mejia name" />
+      <div className="navBar">
+        <img className="name" src="img/name.png" alt="Aaron Mejia name" />
         <div>
-          <img class="profilePic" src="img/aaron.jpg" alt="picture of Aaron sitting on beach watching sunset" />
+          <img className="profilePic" src="img/aaron.jpg" alt="picture of Aaron sitting on beach watching sunset" />
         </div>
-        <br class="wideViewOnly" /><br class="wideViewOnly" />
-        <button type="button" class="expandSection" id="firstNavSection">About</button>
-        <div class="aboutMe-Section">
+        <br className="wideViewOnly" /><br className="wideViewOnly" />
+        <button type="button" className="expandSection" id="firstNavSection" onClick={() => expandNavBarItem("firstNavSection")}>About</button>
+        <div className="aboutMe-Section">
           <p>Hello!<br />I am an up-and-coming developer in the Portland metro area. I come from a background of 5 years in IT, managing the maintenance of large networks, training of technicians, and creation/documentation of processes. Programming excites me and I <em>love</em> helping individuals further themselves and their businesses through technology. I am currently enrolled in <span ><a href="https://epicodus.com" target="_blank">Epicodus</a></span> to help achieve this goal.<br />I'm excited to work with you!</p>
         </div>
-    <br class="wideViewOnly" />
-    <button type="button" class="expandSection">Skills & Interests</button>
-    <div class="aboutMe-Section">
+    <br className="wideViewOnly" />
+    <button type="button" className="expandSection" id="secondNavSection" onClick={() => expandNavBarItem("secondNavSection")}>Skills & Interests</button>
+    <div className="aboutMe-Section">
       <ul>
         <li>I have assisted in the creation & design of at least 4 different websites for small businesses.</li>
         <li>I have built fully-functioning web servers from scratch on Linux using LAMP stacks and WordPress</li>
       </ul>
     </div>
-    <br class="wideViewOnly" />
-    <button type="button" class="expandSection">Contact</button>
-    <div class="aboutMe-Section">
+    <br className="wideViewOnly" />
+    <button type="button" className="expandSection" id="thirdNavSection" onClick={() => expandNavBarItem("thirdNavSection")}>Contact</button>
+    <div className="aboutMe-Section">
       <p>Let's get in touch!</p>
       <p>Email: <a href="mailto:aaron@example.com">aaron@example.com</a>
         <br />
@@ -33,11 +45,11 @@ export default function Portfolio() {
 
   </div>
 
-  <div class="projectSpotlight-Wrapper">
-    <div class="projectSpotlight-Project">
+  <div className="projectSpotlight-Wrapper">
+    <div className="projectSpotlight-Project">
       <h2>Island Resort Website</h2>
       <p>This project was created as an example for a website that could be used by an island resort company.</p>
-      <p class="gitHubLink">GitHub Link: <a href="https://github.com/mejia-dev/sample-island-resort-website">github.com/mejia-dev/sample-island-resort-website</a></p>
+      <p className="gitHubLink">GitHub Link: <a href="https://github.com/mejia-dev/sample-island-resort-website">github.com/mejia-dev/sample-island-resort-website</a></p>
       <img src="img/sample-island-resort.png" alt="screenshot of Island Resort website" />
       <h3>Languages Used:</h3>
       <ol>
@@ -59,10 +71,10 @@ export default function Portfolio() {
 
     <br /><br />
 
-    <div class="projectSpotlight-Project">
+    <div className="projectSpotlight-Project">
       <h2>Blogging Site</h2>
-      <p>An example of a blogging site created by a classmate and I. I styled the CSS on the "About" page.</p>
-      <p class="gitHubLink">GitHub Link: <a href="https://github.com/mejia-dev/blogging-site">github.com/mejia-dev/blogging-site</a></p>
+      <p>An example of a blogging site created by a classNamemate and I. I styled the CSS on the "About" page.</p>
+      <p className="gitHubLink">GitHub Link: <a href="https://github.com/mejia-dev/blogging-site">github.com/mejia-dev/blogging-site</a></p>
       <img src="img/sample-blogging-site.png" alt="screenshot of Blogging website" />
       <h3>Languages Used:</h3>
       <ol>
@@ -84,10 +96,10 @@ export default function Portfolio() {
 
     <br /><br />
 
-    <div class="projectSpotlight-Project">
+    <div className="projectSpotlight-Project">
       <h2>Resume Website</h2>
-      <p>A resume template recreated for the web by a classmate and I.</p>
-      <p class="gitHubLink">GitHub Link: <a href="https://github.com/daisyorozcob/resume-site">github.com/daisyorozcob/resume-site</a></p>
+      <p>A resume template recreated for the web by a classNamemate and I.</p>
+      <p className="gitHubLink">GitHub Link: <a href="https://github.com/daisyorozcob/resume-site">github.com/daisyorozcob/resume-site</a></p>
       <img src="img/sample-resume-site.png" alt="screenshot of Resume website" />
       <h3>Languages Used:</h3>
       <ol>
