9b8ffcf03989a9ce1fecc4bd61c3aad156f9c73d
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-05 14:48:15 -0800

    Pass in props to ProjectSpotlight

--- src/components/ProjectSpotlight.js
+++ src/components/ProjectSpotlight.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function ProjectSpotlight() {
+export default function ProjectSpotlight(props) {
   
   let pagesLink;
   let coAuthorList;
