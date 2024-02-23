8b33452254df3893757c81ead82b43bab7db65ce
mejia-dev <aaron.mejia8484@gmail.com>
2024-02-23 09:53:25 -0800

    Make API call and begin handling rendering

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Portfolio.css';
 import profilePic from '../img/aaron.jpg';
 import nameWordArt from '../img/name.png';
@@ -7,46 +7,61 @@ import ProjectsList from './ProjectsList';
 
 export default function Portfolio() {
 
-  // const [projectsList, setProjectsList] = useState([]);
+  const [projectsListLoaded, setProjectsListLoaded] = useState(false);
+  const [projectsList, setProjectsList] = useState([]);
+  const [projectsListApiError, setProjectsListApiError] = useState(null);
 
-  let projectsList = [
-    {
-      title: "Burlap Coffee Co. Inventory Tracker with Redux",
-      desc: "This project acts as a functional inventory tracker for a coffee company.",
-      linkRepo: "https://github.com/mejia-dev/burlap-coffee-with-redux",
-      linkPages: "https://mejia-dev.github.io/burlap-coffee-with-redux",
-      techsUsed: [
-        "ReactJS",
-        "Redux",
-        "HTML",
-        "CSS",
-        "JavaScript",
-        "webpack",
-        "npm",
-        "Jest",
-        "ESLint",
-        "Babel",
-        "Dotenv",
-      ],
-      features: ["Allows users to add, view, edit, and delete inventory levels of coffee beans.", "Displays warnings if stock is getting low."],
-    },
-    {
-      title: "Epicrust Pizza Parlor",
-      desc: "A simple website to create and order pizzas from the best pizza place in town!",
-      linkRepo: "https://github.com/mejia-dev/pizza-parlor",
-      linkPages: "https://mejia-dev.github.io/pizza-parlor",
-      techsUsed: ["JavaScript","HTML", "CSS"],
-      features: ["Functional order menu", "Functional cart with Create, Read, Update, Delete functionality"],
-    },
-    {
-      title: "Mr. Roboger's Neighborhood",
-      desc: "This webpage is a counting page. Input a number, then a list of numbers counting up (or down) to that number will be displayed. Some numbers will be replaced, according to the defined rules (see Features section).",
-      linkRepo: "https://github.com/mejia-dev/mr-robogers-neighborhood",
-      linkPages: "https://mejia-dev.github.io/mr-robogers-neighborhood",
-      techsUsed: ["JavaScript","HTML", "CSS"],
-      features: [`For numbers that contain a 3, all digits are replaced with "Won't you be my neighbor?"`,'For numbers that contain a 2, all digits are replaced with "Boop!"','For numbers that contain a 1, all digits are replaced with "Beep!"'],
-    },
-  ]
+  useEffect(() => {
+    fetch("https://gh-pinned-repos-api.ysnirix.xyz/api/get?username=mejia-dev")
+      .then(response => response.json())
+      .then((jsonObj) => {
+        setProjectsList(jsonObj.results);
+        setProjectsListLoaded(true);
+        })
+      .catch((error) => {
+        setProjectsListApiError(error)
+        setProjectsListLoaded(true);
+      });
+  }, [])
+
+  // let projectsList = [
+  //   {
+  //     title: "Burlap Coffee Co. Inventory Tracker with Redux",
+  //     desc: "This project acts as a functional inventory tracker for a coffee company.",
+  //     linkRepo: "https://github.com/mejia-dev/burlap-coffee-with-redux",
+  //     linkPages: "https://mejia-dev.github.io/burlap-coffee-with-redux",
+  //     techsUsed: [
+  //       "ReactJS",
+  //       "Redux",
+  //       "HTML",
+  //       "CSS",
+  //       "JavaScript",
+  //       "webpack",
+  //       "npm",
+  //       "Jest",
+  //       "ESLint",
+  //       "Babel",
+  //       "Dotenv",
+  //     ],
+  //     features: ["Allows users to add, view, edit, and delete inventory levels of coffee beans.", "Displays warnings if stock is getting low."],
+  //   },
+  //   {
+  //     title: "Epicrust Pizza Parlor",
+  //     desc: "A simple website to create and order pizzas from the best pizza place in town!",
+  //     linkRepo: "https://github.com/mejia-dev/pizza-parlor",
+  //     linkPages: "https://mejia-dev.github.io/pizza-parlor",
+  //     techsUsed: ["JavaScript","HTML", "CSS"],
+  //     features: ["Functional order menu", "Functional cart with Create, Read, Update, Delete functionality"],
+  //   },
+  //   {
+  //     title: "Mr. Roboger's Neighborhood",
+  //     desc: "This webpage is a counting page. Input a number, then a list of numbers counting up (or down) to that number will be displayed. Some numbers will be replaced, according to the defined rules (see Features section).",
+  //     linkRepo: "https://github.com/mejia-dev/mr-robogers-neighborhood",
+  //     linkPages: "https://mejia-dev.github.io/mr-robogers-neighborhood",
+  //     techsUsed: ["JavaScript","HTML", "CSS"],
+  //     features: [`For numbers that contain a 3, all digits are replaced with "Won't you be my neighbor?"`,'For numbers that contain a 2, all digits are replaced with "Boop!"','For numbers that contain a 1, all digits are replaced with "Beep!"'],
+  //   },
+  // ]
 
   function expandNavBarItem(elementId) {
     document.getElementById(elementId).classList.toggle("active");
@@ -58,18 +73,27 @@ export default function Portfolio() {
     }
   }
 
+  let projectsRendering;
+  if (projectsListApiError){
+    projectsRendering = (
+      <h1>Error while getting results. Please visit <a href="https://github.com/mejia-dev" target="_blank" rel="noreferrer">github.com/mejia-dev</a> to view current projects.</h1>
+    )
+  } else if (!projectsListLoaded) {
+    <h1>Loading, please wait...</h1>
+  }
+
 
   return (
     <React.Fragment>
       <div className="navBar">
         <img className="name" src={nameWordArt} alt="Aaron Mejia name" />
         <div>
-          <img className="profilePic" src={profilePic} alt="picture of Aaron sitting on beach watching sunset" />
+          <img className="profilePic" src={profilePic} alt="silhouette of Aaron sitting on beach watching sunset" />
         </div>
         <br className="wideViewOnly" /><br className="wideViewOnly" />
         <button type="button" className="expandSection" id="firstNavSection" onClick={() => expandNavBarItem("firstNavSection")}>About</button>
         <div className="aboutMe-Section">
-          <p>Hello!<br />I am an up-and-coming developer in the Portland metro area. I come from a background of 5 years in IT, managing the maintenance of large networks, training of technicians, and creation/documentation of processes. Programming excites me and I <em>love</em> helping individuals further themselves and their businesses through technology. I am currently enrolled in <span ><a href="https://epicodus.com" target="_blank">Epicodus</a></span> to help achieve this goal.<br />I'm excited to work with you!</p>
+          <p>Hello!<br />I am an up-and-coming developer in the Portland metro area. I come from a background of 5 years in IT, managing the maintenance of large networks, training of technicians, and creation/documentation of processes. Programming excites me and I <em>love</em> helping individuals further themselves and their businesses through technology. I am currently enrolled in <span ><a href="https://epicodus.com" target="_blank" rel="noreferrer">Epicodus</a></span> to help achieve this goal.<br />I'm excited to work with you!</p>
         </div>
         <br className="wideViewOnly" />
         <button type="button" className="expandSection" id="secondNavSection" onClick={() => expandNavBarItem("secondNavSection")}>Skills & Interests</button>
@@ -90,9 +114,11 @@ export default function Portfolio() {
         </div>
       </div>
 
-      <ProjectsList
+      {/* <ProjectsList
         projectList={projectsList}
-      />
+      /> */}
+      {projectsRendering}
+      
     </React.Fragment>
   )
 }
