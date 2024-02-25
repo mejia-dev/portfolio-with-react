d0d544c5826e1e21eb076d795f42e3fae0fb75a9
mejia-dev <aaron.mejia8484@gmail.com>
2024-02-25 13:46:59 -0800

    Initial test with rendering on two lines

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -3,20 +3,31 @@ import PropTypes from 'prop-types';
 import ProjectSpotlight from './ProjectSpotlight';
 
 export default function ProjectsList(props) {
+  const { projectList } = props;
+  const itemsPerLine = projectList.length <= 3 ? projectList.length : Math.ceil(projectList.length / 2);
+  const lines = [];
+  for (let i = 0; i < 2; i++) {
+    const lineProjects = projectList.slice(i * itemsPerLine, (i + 1) * itemsPerLine);
+    lines.push(lineProjects);
+  }
+
   return (
     <React.Fragment>
       <div className="projectSpotlight-Wrapper">
-        {Object.values(props.projectList).map((project, index) =>
-          <ProjectSpotlight
-            title={project.name}
-            desc={project.description}
-            linkRepo={project.repo}
-            techsUsed={project.language}
-            stars={project.stars}
-            // forks={project.forks}
-            key={index}
-          />
-        )}
+        {lines.map((line, lineIndex) => (
+          <div key={lineIndex} className="line">
+            {line.map((project, index) => (
+              <ProjectSpotlight
+                title={project.name}
+                desc={project.description}
+                linkRepo={project.repo}
+                techsUsed={project.language}
+                stars={project.stars}
+                key={index}
+              />
+            ))}
+          </div>
+        ))}
       </div>
     </React.Fragment>
   )
